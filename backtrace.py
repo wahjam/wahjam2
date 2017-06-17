1c4d7db226aa9cd13ad1894a043a89a74a86961d
Stefan Hajnoczi <stefanha@gmail.com>
2017-06-17 07:07:57 +0100

    Add single address symbol lookup to backtrace.py
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- backtrace.py
+++ backtrace.py
@@ -24,15 +24,21 @@ if image_base is None:
     print('unable to determine ImageBase')
     sys.exit(1)
 
-#  9 0x4007c8bf in vsttest1 (+0xd9c8be) (0x0b05f720)
-for line in sys.stdin:
-    line = line.strip()
-    frame, pc, _, module, _, _ = line.split()
-    if module != 'vsttest1':
-        print(line)
-        continue
-
-    pc = int(pc, 16)
-    addr = pc - mmap_base + image_base
+def resolve(addr):
+    addr = int(addr, 16)
+    addr = addr - mmap_base + image_base
     result = subprocess.run(['i686-w64-mingw32.static.posix-addr2line', '-p', '-f', '-s', '--exe={0}'.format(image), hex(addr)], stdout=subprocess.PIPE)
-    print('{0} {1} {2}'.format(frame, pc, result.stdout.decode().strip()))
+    return '{0} ({1})'.format(result.stdout.decode().strip(), hex(addr))
+
+if len(sys.argv) == 3:
+    print(resolve(sys.argv[2]))
+else:
+    #  9 0x4007c8bf in vsttest1 (+0xd9c8be) (0x0b05f720)
+    for line in sys.stdin:
+        line = line.strip()
+        frame, pc, _, module, _, _ = line.split()
+        if module != 'vsttest1':
+            print(line)
+            continue
+        pretty = resolve(pc)
+        print('{0} {1} {2}'.format(frame, addr, pretty))
