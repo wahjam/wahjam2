fb3674ecd4e57abc5ebe8d39499e0cf85a04ebe8
Stefan Hajnoczi <stefanha@jammr.net>
2020-09-16 22:03:34 +0100

    standalone: add standalone application
    Not all users want a VST plugin. It is also easier to debug a standalone
    application than a plugin.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@jammr.net>

---
+++ standalone/main.cpp
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+#include <QGuiApplication>
+#include <QQmlError>
+#include <QQuickView>
+
+#include "core/global.h"
+
+// TODO AudioProcessor lifecycle and periodic tick
+// TODO PortAudio audio thread that calls processor.process()
+
+static void showViewErrors(QQuickView *view)
+{
+    for (const QQmlError &error : view->errors()) {
+        qDebug("QML error: %s", error.toString().toUtf8().constData());
+    }
+}
+
+int main(int argc, char **argv)
+{
+    installMessageHandler();
+
+    QGuiApplication app(argc, argv);
+
+    globalInit();
+    registerQmlTypes();
+
+    QQuickView *view = new QQuickView{QUrl{"qrc:/qml/application.qml"}};
+    QObject::connect(view, &QQuickView::statusChanged,
+        [=] (QQuickView::Status) { showViewErrors(view); });
+    view->show();
+
+    int rc = app.exec();
+    globalCleanup();
+    return rc;
+}
