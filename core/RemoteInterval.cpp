c7c1c99b52889b90a0a792e43b888605645ffb8c
Stefan Hajnoczi <stefanha@jammr.net>
2020-11-23 09:39:57 +0000

    RemoteInterval: add remote interval downloader
    Add a class for decoding and resampling remote intervals. This will be
    used to play back audio from remote users.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@jammr.net>

---
+++ core/RemoteInterval.cpp
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "RemoteInterval.h"
+
+RemoteInterval::RemoteInterval(const JamConnection::Guid guid_,
+                               const JamConnection::FourCC fourcc_,
+                               int sampleRate_,
+                               QObject *parent)
+    : QObject{parent}, outputSampleRate{44100}, decodeStarted{false},
+      finished{false}
+{
+    memcpy(guid, guid_, sizeof(guid));
+    memcpy(fourcc, fourcc_, sizeof(fourcc));
+}
+
+void RemoteInterval::setSampleRate(int rate)
+{
+    outputSampleRate = rate;
+}
+
+bool RemoteInterval::appendingFinished() const
+{
+    return finished;
+}
+
+// Returns number of output samples
+size_t RemoteInterval::drainResampler(QByteArray *left, QByteArray *right,
+                                      size_t nsamples)
+{
+    size_t n = resampler[CHANNEL_LEFT].resample(left, nsamples);
+    size_t m = resampler[CHANNEL_RIGHT].resample(right, n);
+    if (n != m) {
+        qWarning("Stereo channels out of sync, resamplers produced %zu and %zu samples",
+                 n, m);
+    }
+    return qMin(n, m);
+}
+
+// Returns number of samples filled
+size_t RemoteInterval::fillResampler(size_t nsamples)
+{
+    QByteArray tmpLeft, tmpRight;
+
+    // Estimate how many input samples need to be decoded to produce nsamples
+    // output samples.
+    int inputSampleRate = decodeStarted ? decoder.sampleRate() : 44100;
+    size_t inputSamples = nsamples *
+                          static_cast<double>(inputSampleRate) /
+                          outputSampleRate + 0.5;
+
+    size_t n = decoder.decode(&tmpLeft, &tmpRight, inputSamples);
+    if (n > 0) {
+        double ratio = static_cast<double>(outputSampleRate) /
+                       decoder.sampleRate();
+        resampler[CHANNEL_LEFT].setRatio(ratio);
+        resampler[CHANNEL_RIGHT].setRatio(ratio);
+        decodeStarted = true;
+    }
+
+    // We have no input samples yet, so there is nothing to resample
+    if (!decodeStarted) {
+        return 0;
+    }
+
+    resampler[CHANNEL_LEFT].appendData(tmpLeft);
+    resampler[CHANNEL_RIGHT].appendData(tmpRight);
+    return n;
+}
+
+size_t RemoteInterval::decode(QByteArray *left, QByteArray *right,
+                              size_t nsamples)
+{
+    bool needFill = false;
+    size_t decoded = 0;
+
+    while (nsamples > 0) {
+        size_t filled = 0;
+        if (needFill) {
+            filled = fillResampler(nsamples);
+        }
+
+        size_t n = drainResampler(left, right, nsamples);
+
+        // No input left to decode, stop for now
+        if (n == 0 && needFill && filled == 0) {
+            break;
+        }
+
+        nsamples -= n;
+        decoded += n;
+        needFill = true;
+    }
+
+    return decoded;
+}
+
+void RemoteInterval::appendData(const QByteArray &data)
+{
+    decoder.appendData(data);
+}
+
+void RemoteInterval::finishAppendingData()
+{
+    resampler[CHANNEL_LEFT].finishAppendingData();
+    resampler[CHANNEL_RIGHT].finishAppendingData();
+    finished = true;
+}
---
+++ core/RemoteInterval.h
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include "audio/AudioStream.h"
+#include "JamConnection.h"
+#include "OggVorbisDecoder.h"
+#include "Resampler.h"
+
+// An interval of compressed audio data being downloaded from the server
+//
+// Call appendData() each time compressed audio data is received. Call
+// finishAppendingData() when the download is complete.
+//
+// Decode audio samples by calling decode(). The download may still be in
+// progress and if there is not enough data fewer samples than requested will
+// be returned.
+class RemoteInterval : public QObject
+{
+    Q_OBJECT
+
+public:
+    RemoteInterval(const JamConnection::Guid guid,
+                   const JamConnection::FourCC fourcc,
+                   int sampleRate,
+                   QObject *parent = nullptr);
+
+    // May be called at any time to change the sample rate
+    void setSampleRate(int rate);
+
+    // Returns true if no more samples can be decoded after decode() returns 0
+    bool appendingFinished() const;
+
+    // Fill the stereo left/right channels with up to nsamples of decoded
+    // samples. Returns the number of samples decoded.
+    size_t decode(QByteArray *left, QByteArray *right, size_t nsamples);
+
+public slots:
+    // Add compressed audio data
+    void appendData(const QByteArray &data);
+
+    // No more compressed audio data will be appended
+    void finishAppendingData();
+
+private:
+    OggVorbisDecoder decoder;
+    Resampler resampler[CHANNELS_STEREO];
+    JamConnection::Guid guid;
+    JamConnection::FourCC fourcc;
+    int outputSampleRate;
+    bool decodeStarted;
+    bool finished;
+
+    size_t drainResampler(QByteArray *left, QByteArray *right,
+                          size_t nsamples);
+    size_t fillResampler(size_t nsamples);
+};
