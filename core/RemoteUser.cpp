465c88c88449cc761f590d0ca65475b70cc9fa1e
Stefan Hajnoczi <stefanha@jammr.net>
2021-01-02 10:36:36 +0000

    RemoteUser: add unfinished remote user class
    A remote user manages the audio channels belonging to the user.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@jammr.net>

---
+++ core/RemoteUser.cpp
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "RemoteUser.h"
+
+RemoteUser::RemoteUser(const QString &username,
+                       AppView *appView_,
+                       QObject *parent)
+    : QObject{parent}, appView{appView_}, username_{username}
+{
+}
+
+RemoteUser::~RemoteUser()
+{
+    for (auto channel : qAsConst(channels)) {
+        delete channel;
+    }
+    channels.clear();
+}
+
+QString RemoteUser::username() const
+{
+    return username_;
+}
+
+int RemoteUser::numActiveChannels() const
+{
+    int num = 0;
+    for (auto channel : qAsConst(channels)) {
+        if (channel) {
+            num++;
+        }
+    }
+    return num;
+}
+
+void RemoteUser::setChannelInfo(int channelIndex, const QString &channelName,
+                                bool active)
+{
+    RemoteChannel *channel = channels.value(channelIndex, nullptr);
+
+    // TODO emit notifications for UI?
+    if (channel && !active) {
+        channels.remove(channelIndex);
+        delete channel;
+    } else if (channel && active) {
+        channel->setName(channelName);
+    } else if (!channel && active) {
+        channels.insert(channelIndex,
+                        new RemoteChannel{channelName, appView});
+    }
+}
+
+bool RemoteUser::enqueueRemoteInterval(int channelIndex,
+                                       std::shared_ptr<RemoteInterval> remoteInterval,
+                                       SampleTime nextIntervalTime)
+{
+    RemoteChannel *channel = channels.value(channelIndex, nullptr);
+    if (!channel) {
+        return false;
+    }
+
+    channel->enqueueRemoteInterval(remoteInterval, nextIntervalTime);
+    return true;
+}
---
+++ core/RemoteUser.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include "AppView.h"
+#include "RemoteChannel.h"
+
+class RemoteUser : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString username READ username);
+
+public:
+    RemoteUser(const QString &username,
+               AppView *appView,
+               QObject *parent = nullptr);
+    ~RemoteUser();
+
+    QString username() const;
+    int numActiveChannels() const;
+    void setChannelInfo(int channelIndex, const QString &channelName, bool active);
+
+    // Returns true on success, false on failure
+    bool enqueueRemoteInterval(int channelIndex,
+                               std::shared_ptr<RemoteInterval> remoteInterval,
+                               SampleTime nextIntervalTime);
+
+private:
+    AppView *appView;
+    QString username_;
+    QVector<RemoteChannel*> channels;
+};
