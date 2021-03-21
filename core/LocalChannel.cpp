a95de8de00e490c02708782416d1857e16b288dd
Stefan Hajnoczi <stefanha@jammr.net>
2021-03-21 10:08:03 +0000

    LocalChannel: add local channel for uploading audio
    The local channel represents one channel from a local sound device. It
    compresses audio data and uploads it.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@jammr.net>

---
+++ core/LocalChannel.cpp
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: Apache-2.0
+#include "LocalChannel.h"
+
+LocalChannel::LocalChannel(const QString &name,
+                           AudioStream *captureLeft,
+                           AudioStream *captureRight,
+                           int sampleRate,
+                           SampleTime now,
+                           IIntervalTime *intervalTime_,
+                           QObject *parent)
+    : QObject(parent),
+      intervalTime{intervalTime_},
+      captureStreams{captureLeft, captureRight},
+      name_{name},
+      send_{false},
+      nextSend{true},
+      firstUploadData{true},
+      encoder{1, sampleRate},
+      nextCaptureTime{now}
+{
+}
+
+QString LocalChannel::name() const
+{
+    return name_;
+}
+
+void LocalChannel::setName(const QString &name)
+{
+    name_ = name;
+}
+
+bool LocalChannel::send() const
+{
+    return nextSend;
+}
+
+void LocalChannel::setSend(bool enable)
+{
+    nextSend = enable;
+}
+
+void LocalChannel::processAudioStreams()
+{
+    size_t readable =
+        qMin(captureStreams[CHANNEL_LEFT]->numSamplesReadable(),
+             captureStreams[CHANNEL_RIGHT]->numSamplesReadable());
+
+    // Mix down to mono for now. Don't use AudioStream::readMixStereo() because
+    // that relies on AudioStream::pan. Leave AudioStream::pan alone for now.
+    // Stereo support will be added later and then panning can be done properly.
+    auto left = std::vector<float>(readable);
+    auto right = std::vector<float>(readable);
+    auto samples = std::vector<float>(readable, 0.f);
+    size_t n;
+    n = captureStreams[CHANNEL_LEFT]->read(nextCaptureTime, left.data(), readable);
+    assert(n == readable);
+    n = captureStreams[CHANNEL_RIGHT]->read(nextCaptureTime, right.data(), readable);
+    assert(n == readable);
+    float pan = 0.5f; // linear stereo pan
+    mixSamples(left.data(), samples.data(), readable, pan);
+    mixSamples(right.data(), samples.data(), readable, pan);
+
+    for (size_t i = 0; i < readable; i += n) {
+        size_t remaining = intervalTime->remainingIntervalTime(nextCaptureTime);
+
+        n = qMin(readable, remaining); // only process up to next interval
+        bool lastUploadData = n == remaining;
+
+        if (send_) {
+            QByteArray data = encoder.encode(samples.data() + i, nullptr, n);
+            if (lastUploadData) {
+                data.append(encoder.encode(nullptr, nullptr, 0)); // drain encoder
+            }
+            emit uploadData(guid, data, firstUploadData, lastUploadData);
+            firstUploadData = false;
+        }
+
+        // Next interval
+        if (lastUploadData) {
+            send_ = nextSend;
+            firstUploadData = true;
+            guid = QUuid::createUuid();
+            encoder.reset();
+
+            // Still emit uploadData once per silent interval
+            if (!send_) {
+                emit uploadData(guid, QByteArray{}, true, true);
+            }
+        }
+
+        nextCaptureTime += n;
+    }
+}
---
+++ core/LocalChannel.h
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#include <QUuid>
+#include "audio/AudioStream.h"
+#include "IIntervalTime.h"
+#include "OggVorbisEncoder.h"
+
+/*
+ * An audio channel that processes data from a local sound source. Handles
+ * uploading intervals as well as pan and monitoring.
+ */
+class LocalChannel : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName);
+    Q_PROPERTY(bool send READ send WRITE setSend);
+    // TODO pan, gain, monitoring
+
+public:
+    // Does not take ownership of captureLeft and captureRight
+    LocalChannel(const QString &name,
+                 AudioStream *captureLeft,
+                 AudioStream *captureRight,
+                 int sampleRate,
+                 SampleTime now,
+                 IIntervalTime *intervalTime,
+                 QObject *parent = nullptr);
+
+    QString name() const;
+    void setName(const QString &name);
+    bool send() const;
+    void setSend(bool enable);
+
+public slots:
+    void processAudioStreams();
+
+signals:
+    // Emitted when there is compressed audio data available for uploading.
+    // This signal is emitted at least once per interval. The first time this
+    // signal is emitted each interval 'first' is true. The last time the
+    // signal is emitted each interval 'last' is true. 'data' may be empty.
+    void uploadData(const QUuid &uuid, const QByteArray &data, bool first, bool last);
+
+private:
+    IIntervalTime *intervalTime;
+    AudioStream *captureStreams[CHANNELS_STEREO];
+    QString name_;
+    bool send_;
+    bool nextSend;
+    bool firstUploadData;
+    QUuid guid;
+    OggVorbisEncoder encoder;
+    SampleTime nextCaptureTime;
+};
